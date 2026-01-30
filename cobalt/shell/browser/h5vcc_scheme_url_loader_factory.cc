fca2c2fc0e61d8f0ab6ee83ec37c25e0854fa82a
Sunny Tang <tasunny@google.com>
2026-01-30 14:15:43 -0600

    refractor

--- cobalt/shell/browser/h5vcc_scheme_url_loader_factory.cc
+++ cobalt/shell/browser/h5vcc_scheme_url_loader_factory.cc
@@ -196,7 +196,6 @@ class H5vccSchemeURLLoader : public network::mojom::URLLoader {
     if (base::EndsWith(key, ".html", base::CompareCase::SENSITIVE)) {
       mime_type = "text/html";
     } else if (base::EndsWith(key, ".png", base::CompareCase::SENSITIVE)) {
-      // need to refractor 
       mime_type = "image/png"; 
       if (browser_context_) {
         ReadSplashCache(key, mime_type);
